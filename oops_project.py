ce818f202f505ca8f28740f176e5275c5747df0a
Gowtham5285 <gowthamsaivalluri5285@gmail.com>
2025-09-17 21:20:26 +0530

    version6

--- oops_project.py
+++ oops_project.py
@@ -1,73 +1,121 @@
-class HDFCBankAccount:
-    def __init__(self,username,main_bal):
-        self.name=username
-        self.__MainBal=main_bal
+class SBIBank:
+    def __init__(self,name,acno):
+        self.Name=name
+        self.AccNo=acno
         self.__pin=1234
-        self.__account_Active_Status=True
-     
-    def __verifyPin(self,pin):
+        self.accounStatus=True
+        self.mainBal=5000000
+        self.__isAtmCardHolder=False
+        self.__isCheckBookHolder=False
+        self.__isAtmCardGotFreezed=False
+        self.__isAccountedGotFreezed=False
+
+    def __pinValidation(self,pin):
         return self.__pin==pin
-    
-    def __updateMainBalance(self,amount):
-        self.__MainBal-=amount
-        print(f"You have sucessfully drawn {amount} the balance is {self.__MainBal}")
-
-    def withdraw(self,amount,pin):
-        if self.__account_Active_Status:
-            if self.__verifyPin(pin):
-                if amount>self.__MainBal:
-                    print("you are tring to draw more than you have in your bank account")
-                else:
-                    self.__updateMainBalance(amount)
+        
+    def showBalance(self,pin):
+        if self.accounStatus:
+            if self.__pinValidation(pin):
+                print(f"The main balance is {self.mainBal}")
             else:
-                print("incorrect pin")
-
-    def __updateMainBalByDeopsit(self,amount):
-        self.__MainBal+=amount
-        print(f"you have successfully the amount {amount} the balance is {self.__MainBal}")
-    
-    def deposit(self,amount,pin):
-        if self.__account_Active_Status:
-            if self.__verifyPin(pin):
-                self.__updateMainBalByDeopsit(amount)
-            else:
-                print("incorrect pin")
+                print("Enter the Right Pin")
         else:
-            print("Your account is not in active status")
+            print("Active your Account")
     
-    def __show_MainBal(self,pin):
-        if self.__account_Active_Status:
-            if self.__verifyPin(pin):
-                print(f"the main balance is {self.__MainBal}")
-
-    def __request_for_atmcard_approval(self):
-        pass
+    def __withdrawamount(self,amount):
+        self.mainBal-=amount
+        print(f"the amount is debited successfully the main balance is now {self.mainBal}")
+        
+    def withdraw(self,amount,pin):
+        if self.accounStatus:
+            if self.__pinValidation(pin):
+                if amount>self.mainBal:
+                    print("The amount you want is more than in the account")
+                else:
+                    self.__withdrawamount(amount)
+            else:
+                print("Enter the Right Pin")
+        else:
+            print("Active your Account")
+        
+    def __depositamount(self,amount):
+        self.mainBal+=amount
+        print("Amount Successfully Credited to your Account")
+        print(f"The Balance is {self.mainBal}")
+        
+    def deposit(self,amount,pin):
+        if self.accounStatus:
+            if self.__pinValidation(pin):
+                self.__depositamount(amount)
+            else:
+                print("Enter the Right Pin")
+        else:
+            print("Active your Account")
     
-    def request_for_AccountFreezing(self):
-        pass
-
-    def check_Bal(self,pin):
-        if self.__account_Active_Status:
-            if self.__verifyPin(pin):
-                self.__show_MainBal(pin)
-
+    def __raiseATMCard(self):
+        self.__isAtmCardHolder=True
+        return f"atmcard approved"
+        
+    def request_for_ATMCard(self):
+        Status=self.__raiseATMCard()
+        print(Status)
     
-    def mini_States(self,Pin):
-        pass
+    def __raiseCheckBook(self):
+        self.__isCheckBookHolder=True
+        return f"Checkbook Approved"
+        
+    def requestCheckBook(self):
+        CheckbookStatus=self.__raiseCheckBook()
+        print(CheckbookStatus)
     
-class SavingsAccount(HDFCBankAccount):
-    def __init__(self,name,bal):
-        self.loanLimit=300000 #instance variable of class
-        super().__init__(name,bal)
-
-    def personalLoanRise(self):
-        pass
-
-# class BusinessAccount(HDFCBankAccount):
-#     def businessLoanRise(self):
-#         pass
-sAccount=SavingsAccount("Ram",10000)
-amount=int(input("Enter the amount:-"))
-pin=int(input("Enter the pin:-"))
-sAccount.withdraw(amount,pin)
-sAccount.deposit(amount,pin)
+    def __ATMCardFreezing(self):
+        self.__isAtmCardGotFreezed=True
+        return f"ATM was freezed"
+        
+    def requestforATMCardFreezing(self):
+        ATMFreezingStatus=self.__ATMCardFreezing()
+        print(ATMFreezingStatus)
+    
+    def __AccountFreezing(self):
+        self.__isAccountedGotFreezed=True
+        return f"Account is freezed"
+        
+    def requestforAccountFreezing(self):
+        AccountFreezingStatus=self.__AccountFreezing()
+        print(AccountFreezingStatus)
+        
+class savingsUser(SBIBank):
+    def __init__(self,name,acno):
+        self.loanLimit=300000
+        super().__init__(name,acno)
+    
+    def personalLoan(self,amount):
+        if amount>self.loanLimit:
+            print("Your wanted amount is exceed the loan limit")
+        else:
+            print("Loan amount is sanctioned")
+            
+class CurrentAccount(SBIBank):
+    def __init__(self,name,acno):
+        self.loanLimit=5000000
+        super().__init__(name,acno)
+    
+    def bussinessLoan(self,amount):
+        if amount>self.loanLimit:
+            print("Your wanted amount is exceed the loan limit")
+        else:
+            print("Loan amount will be sanctioned very soon")
+    
+user1=savingsUser("ram",123123123)
+# pin=int(input("Enter the pin:-"))
+# amount=int(input("Enter the amount:-"))
+# user1.showBalance(pin)
+# user1.withdraw(amount,pin)
+# user1.deposit(amount,pin)
+user1.request_for_ATMCard()
+user1.requestCheckBook()
+user1.requestforATMCardFreezing()
+user1.requestforAccountFreezing()
+user1.personalLoan(12345)
+user2=CurrentAccount("ramana",20000)
+user2.requestforAccountFreezing()
