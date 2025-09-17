be8259e0fd38bcfa539b92db70ac148f354b8e89
Gowtham5285 <gowthamsaivalluri5285@gmail.com>
2025-09-17 07:31:45 +0530

    version4

---
+++ oops_project.py
@@ -0,0 +1,30 @@
+class BankHDFC:
+    def __init__(self):
+        pass
+    
+    def __request_for_atmcard_approval(self):
+        pass
+    
+    def __request_for_atmcard_Blocking(self):
+        pass
+    
+    def __request_for_netBanking(self):
+        pass
+    
+    def __request_for_making_individualAccount(self):
+        pass
+    
+    def withdraw(self,Wpin,Wamount):
+        pass
+    
+    def deposit(self,Dpin,Damount):
+        pass
+    
+    def balCheck(self,Bpin):
+        pass
+    
+    def miniStatements(self,Pin):
+        pass
+    
+    
+BankHDFC("vamsi",10000) 
