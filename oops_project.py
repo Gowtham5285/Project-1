e382da95fedeea75c65f6766b850ec552f8c96b8
Gowtham5285 <gowthamsaivalluri5285@gmail.com>
2025-09-17 08:17:25 +0530

    version4

--- oops_project.py
+++ oops_project.py
@@ -1,7 +1,43 @@
-class BankHDFC:
-    def __init__(self):
-        pass
+class HDFCBankAccount:
+    def __init__(self,username,main_bal):
+        self.name=username
+        self.__MainBal=main_bal
+        self.__pin=1234
+        self.__account_Active_Status=True
+     
+    def __verifyPin(self,pin):
+        return self.__pin==pin
     
+    def __updateMainBalance(self,amount):
+        self.__MainBal-=amount
+        print(f"You have sucessfully drawn {amount} the balance is {self.__MainBal}")
+
+    def withdraw(self,amount,pin):
+        if self.__account_Active_Status:
+            if self.__verifyPin(pin):
+                if amount>self.__MainBal:
+                    print("you are tring to draw more than you have in your bank account")
+                else:
+                    self.__updateMainBalance(amount)
+            else:
+                print("incorrect pin")
+                
+    def __updateMainBalByDeopsit(self,amount):
+        self.__MainBal+=amount
+        print(f"you have successfully the amount {amount} the balance is {self.__MainBal}")
+    
+    def deposit(self,amount,pin):
+        if self.__account_Active_Status:
+            if self.__verifyPin(pin):
+                self.__updateMainBalByDeopsit(amount)
+            else:
+                print("incorrect pin")
+        else:
+            print("Your account is not in active status")
+    
+    def __show_MainBal(pin,self):
+        if self.__
+
     def __request_for_atmcard_approval(self):
         pass
     
@@ -11,20 +47,34 @@ class BankHDFC:
     def __request_for_netBanking(self):
         pass
     
+    def request_for_AccountFreezing(self):
+        pass
+
     def __request_for_making_individualAccount(self):
         pass
+
+    def check_Bal(self,pin):
+        if self.__account_Active_Status:
+            if self.__verifyPin(pin):
+                self.__show_MainBal(pin)
+
     
-    def withdraw(self,Wpin,Wamount):
+    def mini_States(self,Pin):
         pass
     
-    def deposit(self,Dpin,Damount):
+class SavingsAccount(HDFCBankAccount):
+    def __init__(self,name,bal):
+        self.loanLimit=300000 #instance variable of class
+        super().__init__(name,bal)
+
+    def personalLoanRise(self):
         pass
-    
-    def balCheck(self,Bpin):
-        pass
-    
-    def miniStatements(self,Pin):
-        pass
-    
-    
-BankHDFC("vamsi",10000) 
+
+# class BusinessAccount(HDFCBankAccount):
+#     def businessLoanRise(self):
+#         pass
+sAccount=SavingsAccount("Ram",10000)
+amount=int(input("Enter the amount:-"))
+pin=int(input("Enter the pin:-"))
+sAccount.withdraw(amount,pin)
+sAccount.deposit(amount,pin)
